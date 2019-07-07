09ddb6e51d8a64e3e84c1c847c2daafd7254750b
SianLouisa <sianlouisa@hotmail.com>
2019-07-07 22:25:34 +0100

    added fetch users, sort, search on change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card-list';
+import { SearchField } from './components/search-field/SearchField';
+import { SortOptions } from './components/sort-options/SortOptions';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: '',
+      isSortedAscending: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users =>
+        this.setState({ monsters: users }, () =>
+          console.log(this.state.monsters),
+        ),
+      )
+      .catch(err => console.log(err));
+  }
+
+  compareNamesAscending = (a, b) => {
+    const nameA = a.name.toLowerCase();
+    const nameB = b.name.toLowerCase();
+
+    let comparison = 0;
+    if (nameA > nameB) {
+      comparison = 1;
+    } else if (nameA < nameB) {
+      comparison = -1;
+    }
+    return comparison;
+  };
+
+  compareNamesDescending = (a, b) => {
+    const nameA = a.name.toLowerCase();
+    const nameB = b.name.toLowerCase();
+
+    let comparison = 0;
+    if (nameA < nameB) {
+      comparison = 1;
+    } else if (nameA > nameB) {
+      comparison = -1;
+    }
+    return comparison;
+  };
+
+  onChange = e => this.setState({ searchField: e.target.value });
+
+  handleSort = e => {
+    const { monsters, isSortedAscending } = this.state;
+    if (!isSortedAscending) {
+      const sortedMonsters = monsters.sort(this.compareNamesAscending);
+      this.setState({ monsters: sortedMonsters, isSortedAscending: true });
+    } else {
+      const sortedMonsters = monsters.sort(this.compareNamesDescending);
+      this.setState({ monsters: sortedMonsters, isSortedAscending: false });
+    }
+  };
+
+  render() {
+    const { monsters, searchField, isSortedAscending } = this.state;
+
+    const filteredMonsters = monsters.filter(monster => {
+      const name = monster.name.toLowerCase();
+      const email = monster.email.toLowerCase();
+      const search = searchField.toLowerCase();
+      return name.includes(search) || email.includes(search);
+    });
+
+    return (
+      <div className="App">
+        <SearchField handleSearchChange={this.onChange} />
+        <SortOptions
+          handleSort={this.handleSort}
+          isSortedAscending={isSortedAscending}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
