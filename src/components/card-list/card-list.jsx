cac5475d62f9401f58d46e922fc3cfec88e14020
SianLouisa <sianlouisa@hotmail.com>
2019-07-08 19:03:13 +0100

    message if no users found

--- src/components/card-list/card-list.jsx
+++ src/components/card-list/card-list.jsx
@@ -3,6 +3,10 @@ import './card-list.css';
 import { Card } from '../card/card';
 
 export const CardList = ({ monsters }) => {
+  if (monsters.length <= 0) {
+    return <h1 style={{ color: '#ff8fa0' }}>No users found :(</h1>;
+  }
+
   return (
     <div className="card-list">
       {monsters.map(monster => (
