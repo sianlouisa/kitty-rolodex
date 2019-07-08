3dce4e53c771230298a546534cb3c87ebe4576ae
SianLouisa <sianlouisa@hotmail.com>
2019-07-08 19:50:20 +0100

    add files for gh-pages

---
+++ build/precache-manifest.393bd36f4be4d652eda2501a2763d441.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca5bd66122a07493dc78dea07f3382d",
+    "url": "/kitty-rolodex/index.html"
+  },
+  {
+    "revision": "b33ad450a3e811898c77",
+    "url": "/kitty-rolodex/static/css/main.e428a39d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/kitty-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b33ad450a3e811898c77",
+    "url": "/kitty-rolodex/static/js/main.6272184c.chunk.js"
+  },
+  {
+    "revision": "5dda48e9787a66231d26",
+    "url": "/kitty-rolodex/static/js/runtime~main.e270939c.js"
+  }
+]);
